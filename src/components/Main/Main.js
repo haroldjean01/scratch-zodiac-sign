0dfd913daca847befaa3f18a4b32510b8b503ac7
Harold Jean <haroldjean38@yahoo.com>
2023-01-09 20:30:13 -0500

    added a components folder with header footer main folder inside

---
+++ src/components/Main/Main.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Main.css';
+import background from '../../background.png';
+import Animal from '../Animal/Animal.js';
+import { animals } from '../../data.js';
+
+export default function Main() {
+  return (
+    <main className="main" style={{ backgroundImage: `url(${background})` }}>
+      {animals.map((animal) => (
+        <Animal
+          key={animal.type}
+          name={animal.name}
+          image={animal.type}
+          url={animal.url}
+          says={animal.says}
+          top={animal.top}
+          left={animal.left}
+        />
+      ))}
+    </main>
+  );
+}
